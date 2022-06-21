e131e8968634800409ec21ecdd94ffd9fd4b1b4b
jeonghs2015 <hsjung201558@gmail.com>
2022-06-21 12:22:39 +0900

    g5-116 검색창 오류 수정

--- theme/everyday/mobile/shop/shop.head.php
+++ theme/everyday/mobile/shop/shop.head.php
@@ -1,6 +1,8 @@
 <?php
 if (!defined("_GNUBOARD_")) exit; // 개별 페이지 접근 불가
 
+$q = isset($_GET['q']) ? clean_xss_tags($_GET['q'], 1, 1) : '';
+
 include_once(G5_THEME_PATH.'/head.sub.php');
 include_once(G5_LIB_PATH.'/outlogin.lib.php');
 include_once(G5_LIB_PATH.'/visit.lib.php');
