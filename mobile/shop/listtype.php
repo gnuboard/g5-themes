d3f3eb191980a986423e74672bdc4a36e02449d5
seeoya <seeoya@naver.com>
2022-06-21 16:41:05 +0900

    세일 > 할인으로 문구 수정

--- mobile/shop/listtype.php
+++ mobile/shop/listtype.php
@@ -6,7 +6,7 @@ if ($type == 1)      $g5['title'] = '히트상품';
 else if ($type == 2) $g5['title'] = '추천상품';
 else if ($type == 3) $g5['title'] = '최신상품';
 else if ($type == 4) $g5['title'] = '인기상품';
-else if ($type == 5) $g5['title'] = '세일상품';
+else if ($type == 5) $g5['title'] = '할인상품';
 else
     alert('상품유형이 아닙니다.');
 
