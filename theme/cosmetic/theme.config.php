7270219877d42ce54c3e5cb18a586252ccbb5f35
jeonghs2015 <hsjung201558@gmail.com>
2022-06-21 16:22:18 +0900

    g5-122 theme config 파일 수정

--- theme/cosmetic/theme.config.php
+++ theme/cosmetic/theme.config.php
@@ -4,7 +4,7 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 // 테마가 지원하는 장치 설정 pc, mobile
 // 선언하지 않거나 값을 지정하지 않으면 그누보드5의 설정을 따른다.
 // G5_SET_DEVICE 상수 설정 보다 우선 적용됨
-if(! defined('G5_THEME_DEVICE')) define('G5_THEME_DEVICE', 'PC');
+if(! defined('G5_THEME_DEVICE')) define('G5_THEME_DEVICE', '');
 
 // 테마에서 커뮤니티 지원여부 설정
 // 커뮤니티 사용없이 쇼핑몰이 초기화면이라면 false로 설정
@@ -150,8 +150,8 @@ $theme_config = array(
 if(!defined('G5_IS_ADMIN'))
     include_once(G5_THEME_LIB_PATH.'/theme.lib.php');
 
-define('G5_THEME_ADM_DIR', 'theme_adm');
-define('G5_THEME_ADM_URL', G5_THEME_URL.'/'.G5_THEME_ADM_DIR);
+    define('G5_THEME_ADM_DIR', '/theme/cosmetic/theme_adm/footerinfo.php');
+    define('G5_THEME_ADM_URL', G5_URL.'/'.G5_THEME_ADM_DIR);
 
 
 ?>
