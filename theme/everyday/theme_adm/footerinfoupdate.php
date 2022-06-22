678f7c9e128aa4a524bc7fd580fb8195c232589f
jeonghs2015 <hsjung201558@gmail.com>
2022-06-22 10:13:43 +0900

    g5-116 data 생성 폴더 접근 권한 수정

--- theme/everyday/theme_adm/footerinfoupdate.php
+++ theme/everyday/theme_adm/footerinfoupdate.php
@@ -1,10 +1,10 @@
 <?php
 include_once('./_common.php');
 
-@mkdir(G5_DATA_PATH."/cache/theme", G5_DIR_PERMISSION);
-@chmod(G5_DATA_PATH."/cache/theme", G5_DIR_PERMISSION);
-@mkdir(G5_DATA_PATH."/cache/theme/everyday", G5_DIR_PERMISSION);
-@chmod(G5_DATA_PATH."/cache/theme/everyday", G5_DIR_PERMISSION);
+@mkdir(G5_DATA_PATH."/theme", '777');
+@chmod(G5_DATA_PATH."/theme", '777');
+@mkdir(G5_DATA_PATH."/theme/everyday", '777');
+@chmod(G5_DATA_PATH."/theme/everyday", '777');
 
 $data = array();
 
@@ -27,7 +27,7 @@ if(!$etc)
 
 $data = array('tel' => $tel, 'etc' => $etc, 'depositor' => $depositor, 'account' => $account);
 
-$save_file = G5_DATA_PATH.'/cache/theme/everyday/footerinfo.php';
+$save_file = G5_DATA_PATH.'/theme/everyday/footerinfo.php';
 
 // 캐시파일로 저장
 $cache_fwrite = true;
