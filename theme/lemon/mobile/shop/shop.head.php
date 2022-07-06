ab65ce50eb668c61e17fec1ca327e92204e253ce
jeonghs2015 <hsjung201558@gmail.com>
2022-07-06 17:16:50 +0900

    g5-124 lemon 테마 다운로드

---
+++ theme/lemon/mobile/shop/shop.head.php
@@ -0,0 +1,35 @@
+<?php
+if (!defined("_GNUBOARD_")) exit; // 개별 페이지 접근 불가
+
+include_once(G5_THEME_PATH.'/head.sub.php');
+include_once(G5_LIB_PATH.'/outlogin.lib.php');
+include_once(G5_LIB_PATH.'/visit.lib.php');
+include_once(G5_LIB_PATH.'/connect.lib.php');
+include_once(G5_LIB_PATH.'/popular.lib.php');
+include_once(G5_LIB_PATH.'/latest.lib.php');
+?>
+
+<header id="hd">
+    <?php if ((!$bo_table || $w == 's' ) && defined('_INDEX_')) { ?><h1><?php echo $config['cf_title'] ?></h1><?php } ?>
+
+    <div id="skip_to_container"><a href="#container">본문 바로가기</a></div>
+
+    <?php if(defined('_INDEX_')) { // index에서만 실행
+        include G5_MOBILE_PATH.'/newwin.inc.php'; // 팝업레이어
+    } ?>
+
+    <div id="hd_wr">
+        <div id="logo"><a href="<?php echo G5_SHOP_URL; ?>/"><img src="<?php echo G5_DATA_URL; ?>/common/mobile_logo_img" alt="<?php echo $config['cf_title']; ?> 메인"></a></div>
+        <div id="hd_btn">
+            <button type="button" id="btn_hdcate" class="cate_btn"><i class="fa fa-bars" aria-hidden="true"></i><span class="sound_only">분류</span></button>
+            <a href="<?php echo G5_SHOP_URL; ?>/cart.php"><i class="fa fa-shopping-cart" aria-hidden="true"></i><span class="sound_only">장바구니</span><span class="cart-count"><?php echo get_boxcart_datas_count(); ?></span></a>
+
+        </div>
+    </div>
+    <?php include_once(G5_THEME_MSHOP_PATH.'/category.php'); // 분류 ?>
+
+
+</header>
+
+<div id="container">
+    <?php if (!defined("_INDEX_")) { ?><h1 id="container_title"><?php echo $g5['title'] ?><a href="javascript:history.back()" class="btn_back"><i class="fa fa-angle-left"></i><span class="sound_only">뒤로</span></a></h1><?php } ?>
