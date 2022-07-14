7bb25c7234286047f29d48daf272573c2e9fe84c
jeonghs2015 <hsjung201558@gmail.com>
2022-07-14 10:39:35 +0900

    g5-130 테마관리 sns 로그인 api 링크 오류 수정

--- theme/hotsauce/theme_adm/snslogin.php
+++ theme/hotsauce/theme_adm/snslogin.php
@@ -18,28 +18,28 @@ include_once('./head.php');
         <li>
             <h3>네이버로그인 API 정보</h3>
             <label for="naver_id">ID</label>
-            <input type="text" name="naver_id" id="naver_id" class="frm_input" value="<?php echo get_text($snslogin['naver_id']); ?>"><br>
+            <input type="text" name="naver_id" id="naver_id" class="frm_input" value="<?php if(isset($snslogin['naver_id']))echo get_text($snslogin['naver_id']); ?>"><br>
             <label for="naver_key">KEY</label>
-            <input type="text" name="naver_key" id="naver_key" class="frm_input" value="<?php echo get_text($snslogin['naver_key']); ?>">
+            <input type="text" name="naver_key" id="naver_key" class="frm_input" value="<?php if(isset($snslogin['naver_key']))echo get_text($snslogin['naver_key']); ?>">
         </li>
         <li>
             <h3>카카오로그인 API 정보</h3>
             <label for="kakao_key">KEY</label>
-            <input type="text" name="kakao_key" id="kakao_key" class="frm_input" value="<?php echo get_text($snslogin['kakao_key']); ?>">
+            <input type="text" name="kakao_key" id="kakao_key" class="frm_input" value="<?php if(isset($snslogin['kakao_key']))echo get_text($snslogin['kakao_key']); ?>">
         </li>
         <li>
             <h3>페이스북로그인 API 정보</h3>
             <label for="facebook_id">ID</label>
-            <input type="text" name="facebook_id" id="facebook_id" class="frm_input" value="<?php echo get_text($snslogin['facebook_id']); ?>"><br>
+            <input type="text" name="facebook_id" id="facebook_id" class="frm_input" value="<?php if(isset($snslogin['facebook_id']))echo get_text($snslogin['facebook_id']); ?>"><br>
             <label for="facebook_key">KEY</label>
-            <input type="text" name="facebook_key" id="facebook_key" class="frm_input" value="<?php echo get_text($snslogin['facebook_key']); ?>">
+            <input type="text" name="facebook_key" id="facebook_key" class="frm_input" value="<?php if(isset($snslogin['facebook_key']))echo get_text($snslogin['facebook_key']); ?>">
         </li>
         <li>
             <h3>구글+로그인 API 정보</h3>
             <label for="google_id">ID</label>
-            <input type="text" name="google_id" id="google_id" class="frm_input" value="<?php echo get_text($snslogin['google_id']); ?>"><br>
+            <input type="text" name="google_id" id="google_id" class="frm_input" value="<?php if(isset($snslogin['google_id']))echo get_text($snslogin['google_id']); ?>"><br>
             <label for="google_key">KEY</label>
-            <input type="text" name="google_key" id="google_key" class="frm_input" value="<?php echo get_text($snslogin['google_key']); ?>">
+            <input type="text" name="google_key" id="google_key" class="frm_input" value="<?php if(isset($snslogin['google_key']))echo get_text($snslogin['google_key']); ?>">
         </li>
     </ul>
 </div>
