f6658096571ea22e2899e6240c5406235c1f2aec
seeoya <seeoya@naver.com>
2022-07-22 12:25:59 +0900

    스타일 수정

--- theme/buzinga/mobile/skin/outlogin/shop_basic/outlogin.skin.2.php
+++ theme/buzinga/mobile/skin/outlogin/shop_basic/outlogin.skin.2.php
@@ -9,8 +9,8 @@ $cp_count = 0;
 $sql = " select cp_id
             from {$g5['g5_shop_coupon_table']}
             where mb_id IN ( '{$member['mb_id']}', '전체회원' )
-              and cp_start <= '".G5_TIME_YMD."'
-              and cp_end >= '".G5_TIME_YMD."' ";
+            and cp_start <= '".G5_TIME_YMD."'
+            and cp_end >= '".G5_TIME_YMD."' ";
 $res = sql_query($sql);
 
 for($k=0; $cp=sql_fetch_array($res); $k++) {
@@ -25,17 +25,17 @@ for($k=0; $cp=sql_fetch_array($res); $k++) {
     <button type="button" class="btn_close"><i class="fa fa-times"></i><span class="sound_only">닫기</span></button>
     <div class="ol_wr">
         <div class="ol_hd">
-	        <span class="profile_img">
-	            <?php echo get_member_profile_img($member['mb_id']); ?>
-	            <a href="<?php echo G5_BBS_URL ?>/member_confirm.php?url=register_form.php" id="ol_after_info"><i class="fa fa-cog" aria-hidden="true"></i><span class="sound_only">정보수정</span></a>
-	        </span>
-	        <strong><?php echo $nick ?>님</strong>
-	        <div class="ol_hd_btn">
-	        	<a href="<?php echo G5_BBS_URL ?>/logout.php" id="ol_after_logout" class="btn_b01 btn_s">로그아웃</a>
-	    		<a href="<?php echo G5_THEME_ADM_URL ?>" target="_blank" class="btn_admin">테마관리</a>
-	    	</div>
-    	</div>
-    	<ul id="ol_after_private">
+            <span class="profile_img">
+                <?php echo get_member_profile_img($member['mb_id']); ?>
+                <a href="<?php echo G5_BBS_URL ?>/member_confirm.php?url=register_form.php" id="ol_after_info"><i class="fa fa-cog" aria-hidden="true"></i><span class="sound_only">정보수정</span></a>
+            </span>
+            <strong><?php echo $nick ?>님</strong>
+            <div class="ol_hd_btn">
+                <a href="<?php echo G5_BBS_URL ?>/logout.php" id="ol_after_logout" class="btn_b01 btn_s">로그아웃</a>
+                <?php if($is_admin) { ?><a href="<?php echo G5_THEME_ADM_URL ?>" target="_blank" class="btn_admin">테마관리</a><?php } ?>
+            </div>
+        </div>
+        <ul id="ol_after_private">
             <li>
                 <a href="<?php echo G5_BBS_URL ?>/point.php" target="_blank" id="ol_after_pt" class="win_point"><i class="fas fa-database"></i> 포인트<strong><?php echo $point ?> 점</strong>
                 </a>
@@ -52,9 +52,9 @@ for($k=0; $cp=sql_fetch_array($res); $k++) {
             <li><a href="<?php echo G5_SHOP_URL; ?>/couponzone.php"><i class="fas fa-gift"></i>쿠폰존</a></li>
             <?php if ($is_admin) {  ?>
             <li><a href="<?php echo G5_ADMIN_URL ?>/shop_admin/"><i class="fas fa-user-cog"></i>관리자</a></li>
-	        <?php } ?>
+            <?php } ?>
         </ul>
- 	</div>
+    </div>
 </section>       
 
 <script>
